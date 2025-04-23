9ba8c5a962018fb08300d746f61caf9adcd25919
nikhiltmg <153078308+NikhilTamang@users.noreply.github.com>
2025-04-23 23:13:34 +0545

    Introduction to JavaScript

---
+++ Program 54/new.js
@@ -0,0 +1,3 @@
+console.log("Hello Earth");
+console.log("Console is Running...");
+console.log("I am Running as well......");
---
+++ Program 54/script.js
@@ -0,0 +1,21 @@
+alert("yooo broo");
+
+console.log("Hey I am Console");
+console.log("Console is Running...");
+console.log("I am Running as well......");
+
+var a = prompt("Enter your name bro");
+console.log("Your name is " + a);
+
+var isTrue = confirm(
+  "Are you sure you want to leave my JavaScript Intro Website");
+
+if (isTrue) {
+  console.log("Computer is Blasting");
+} else {
+  console.log("Computer is not blasting");
+}
+
+document.title = "Hey i am good"
+
+document.body.style.backgroundColor = "red"
