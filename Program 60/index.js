fb43e5e433935e860dfa506ea0386e78c55c865b
NikhilTamang <nikhilwaiba55@gmail.com>
2025-05-04 23:12:25 +0545

    JavaScript Strings

---
+++ Program 60/index.js
@@ -0,0 +1,29 @@
+let myname = "Nikhil";
+console.log(myname[0]);
+console.log(myname[1]);
+console.log(myname[2]);
+console.log(myname[3]);
+console.log(myname[4]);
+console.log(myname[5]);
+
+console.log( "The length of the variable myname is : ",myname.length)
+
+
+let a ="Ninja";
+let b ="Hattori";
+console.log("His full name is "+ a + " "+b);
+console.log(`His full name is ${a} ${b}`);
+
+let c = "meSSi"
+let caste = "Tamang"
+console.log(c.toUpperCase())
+console.log(c.toLowerCase())
+console.log(c.length)
+console.log(c.slice(0,4))
+console.log(c.slice(1))
+console.log(c.replace("SS", "10"))
+console.log(c.concat(caste, "Waiba", "Samdhi"))
+
+let nameWithSpaces = "   meSSi   ";
+console.log("Before trim: '" + nameWithSpaces + "'");
+console.log("After trim: '" + nameWithSpaces.trim() + "'");
