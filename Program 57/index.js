5a7aedafc0336ee2ea8274e52718da18d9e48def
NikhilTamang <nikhilwaiba55@gmail.com>
2025-05-04 15:16:34 +0545

    Learnt JavaScript Loops

---
+++ Program 57/index.js
@@ -0,0 +1,40 @@
+// let a = 0;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+    
+// }
+
+
+// let obj ={
+//     name : "Nikhil",
+//     role : "Student ",
+//     goal : "Software Engineer",
+// }
+
+// for (const key in obj) {    // This is for..in loop
+//         let element = obj[key];
+//         console.log(key, element)
+    
+// }
+
+// for (const b of "Nikhil Tamang") {   //this is for...of loop
+//     console.log(b)
+// }
+
+
+// let i=0;        //this is a while loop
+// while(i<10){
+//     console.log(i)
+//     i++;
+// }
+
+
+
+
+
+let z = 10;
+do {                 // this is a do while loop
+    console.log(z);
+    z--;
+} while (z>0);
