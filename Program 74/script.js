3b7ea22b5803528c6e139cf04d7c135ef78ca5c5
NikhilTamang <nikhilwaiba55@gmail.com>
2025-05-17 15:35:58 +0545

    Mouse and key events

--- Program 74/script.js
+++ Program 74/script.js
@@ -1,5 +1,24 @@
-let button = document.getElementById("btn")
+let button = document.getElementById("btn");
 
-button.addEventListener("click", () =>{
-    alert("You clicked a button.")
-})
+// Fired when a pointing device button (e.g., a mouse's primary button) is pressed and released on a single element.
+button.addEventListener("click", () => {
+  // alert("You clicked a button.")
+  document.querySelector(".box").innerHTML =
+    " <b>A button was clicked.</b> Thanks for clicking.";
+});
+
+// Fired when a pointing device button (e.g., a mouse's primary button) is clicked twice on a single element.
+button.addEventListener("dblclick", () => {
+  document.querySelector(".box").innerHTML =
+    " <b> Bro you clicked the button twice.</b>";
+});
+
+// Fired when the user attempts to open a context menu.
+button.addEventListener("contextmenu", () => {
+  alert("Why did you right click that button?");
+});
+
+// Fired when a key is pressed.
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+});
